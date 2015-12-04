6a6b5e129989dabf8e94fb57f80c01c405c45b58
Alex Rusin <alexrusin_2000@yahoo.com>
2015-12-03 23:23:54 -0800

    Created basic chat functionality

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+var socket = io();
+socket.on('connect', function(){
+	console.log ('Connected to the sever');
+});
+
+socket.on('message', function(message){
+	console.log('New message');
+	console.log(message.text);
+
+	jQuery('.messages').append('<p>'+ message.text+'</p>');
+});
+
+// Handles submitting of new message
+var $form = jQuery('#message-form');
+var $message = $form.find('input[name=message]');
+
+$form.on('submit', function(event){
+	event.preventDefault();
+
+	socket.emit('message', {
+		text: $message.val()
+	});
+
+	$message.val('').focus();
+});
